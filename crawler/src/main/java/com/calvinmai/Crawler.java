14072a278f0cef2b0245cf953aec9256058bfe08
Calvin Mai <calvin.mai@gmail.com>
2018-08-16 18:19:47 -0700

    added more unit tests

--- crawler/src/main/java/com/calvinmai/Crawler.java
+++ crawler/src/main/java/com/calvinmai/Crawler.java
@@ -17,6 +17,22 @@ public class Crawler {
 
     private CrawlResult result = new CrawlResult();
 
+    //getters
+    public String getMessage()
+    {
+        return this.message;
+    }
+
+    public boolean getHasRun()
+    {
+        return this.hasRun;
+    }
+
+    public boolean getIsRunning()
+    {
+        return this.isRunning;
+    }
+
     public Crawler (String startEndPoint)
     {
         this.startEndPoint = startEndPoint;
@@ -30,45 +46,47 @@ public class Crawler {
             {
                 this.isRunning = true;
                 Response response = HttpClient.getInstance().asyncGetWithFullUrl(this.startEndPoint);
-                String content = response.readEntity(String.class);
-                System.out.println("content=\n" + content);
-                ObjectMapper objectMapper = new ObjectMapper();
-                //objectMapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
-                try {
-                   // ResponseData responseData = objectMapper.readValue(content, ResponseData.class);
-                   // if(responseData.getLinks() != null)
-                   //     this.message = "" + response.getLinks().size() + " link(s) returned";
+                if(response.getStatus() == 200) {
+                    String content = response.readEntity(String.class);
+                    System.out.println("content=\n" + content);
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    //objectMapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
+                    try {
+                        // ResponseData responseData = objectMapper.readValue(content, ResponseData.class);
+                        // if(responseData.getLinks() != null)
+                        //     this.message = "" + response.getLinks().size() + " link(s) returned";
 
-                    JsonNode rootNode = objectMapper.readTree(content);
+                        JsonNode rootNode = objectMapper.readTree(content);
 
-                    JsonNode linksNode = rootNode.path("links");
-                    Iterator<JsonNode> links = linksNode.elements();
-                    while(links.hasNext())
-                    {
-                        JsonNode link = links.next();
-                        String s = link.asText();
-                        this.result.totalNumOfRequests++;
+                        JsonNode linksNode = rootNode.path("links");
+                        Iterator<JsonNode> links = linksNode.elements();
+                        while (links.hasNext()) {
+                            JsonNode link = links.next();
+                            String s = link.asText();
+                            this.result.totalNumOfRequests++;
 
-                        if(this.result.requests.containsKey(s))
-                            this.result.totalNumOfDuplicateRequests++;
-                        else
-                        {
-                            Response subResponse = HttpClient.getInstance().asyncGetWithFullUrl(s);
-                            if(subResponse.getStatus() >= 400)
-                                this.result.totalNumOfFailedRequests++;
-                            else
-                                this.result.totalNumOfSuccessfulRequests++;
+                            if (this.result.requests.containsKey(s))
+                                this.result.totalNumOfDuplicateRequests++;
+                            else {
+                                Response subResponse = HttpClient.getInstance().asyncGetWithFullUrl(s);
+                                if (subResponse.getStatus() >= 400)
+                                    this.result.totalNumOfFailedRequests++;
+                                else
+                                    this.result.totalNumOfSuccessfulRequests++;
 
-                            this.result.requests.put(s, subResponse.getStatus());
+                                this.result.requests.put(s, subResponse.getStatus());
+                            }
+
                         }
-
+                        this.message = "Crawler completed successfully.";
+                    } catch (Exception ex) {
+                        this.message = "Crawler completed with error. Message: " + ex.getMessage();
+                        ex.printStackTrace();
                     }
-                    this.message = "Crawler completed successfully.";
                 }
-                catch (Exception ex)
+                else
                 {
-                    this.message = "Crawler completed with error. Message: " + ex.getMessage();
-                    ex.printStackTrace();
+                    this.message = "Crawler completed with error. Message: Entry point error. Code : " + response.getStatus();
                 }
                 this.hasRun = true;
                 this.isRunning = false;
--- crawler/src/test/java/com/calvinmai/CrawlerTest.java
+++ crawler/src/test/java/com/calvinmai/CrawlerTest.java
@@ -1,17 +1,25 @@
 package com.calvinmai;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import javax.ws.rs.core.Response;
 import java.lang.reflect.Field;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class CrawlerTest {
+    // sut = System Under Test
+    private Crawler sut = new Crawler("https://raw.githubusercontent.com/OnAssignment/compass-interview/master/data.json");
+
+    // All external resources ( to sut/crawler) need to be mocked.
     @Mock
     private HttpClient mockHttpClient;
 
@@ -22,7 +30,7 @@ public class CrawlerTest {
 
         when(mockHttpClient.toString()).thenReturn("MockedHttpClient");
 
-        //setMock(mockHttpClient);
+        setMock(mockHttpClient);
     }
     private void setMock(HttpClient mock) {
         try {
@@ -49,5 +57,17 @@ public class CrawlerTest {
         String s = HttpClient.getInstance().toString();
 
         assertEquals("MockedHttpClient", s);
+    }
+
+    @Test
+    public void whenEntryPointIsDownCrawlerShouldEndWithMessageContainsHttpCode(){
+        Response.ResponseBuilder responseBuilder = Response.status(500);
+        //responseBuilder.entity("this is the payload.").build()
+        when(mockHttpClient.asyncGetWithFullUrl(any(String.class))).thenReturn(responseBuilder.build());
+
+        this.sut.run();
+        assertEquals(true, this.sut.getHasRun());
+        assertEquals(false, this.sut.getIsRunning());
+        Assert.assertThat(this.sut.getMessage(), CoreMatchers.containsString("500"));
     }
 }
